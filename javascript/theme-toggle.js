115930384275fcc90ac0bdda5046c325ae2178df
Jayan Ghimire <JayanGhimire@example.com>
2024-06-04 10:35:17 +0545

    final assignment

--- javascript/theme-toggle.js
+++ javascript/theme-toggle.js
@@ -1,6 +1,12 @@
 var toggleTheme = () => {
-    console.log("hello");
+  
     var isDarkMode = document.documentElement.classList.toggle('dark-mode');
+    if(document.documentElement.classList.contains('dark-mode')){
+        image.innerHTML='🌝'
+    }
+    else{
+        image.innerHTML='🌚'
+    }
     
  
    
@@ -8,5 +14,3 @@ var toggleTheme = () => {
 
 var image = document.querySelector('.theme-toggler');
 image.addEventListener('click', toggleTheme);
-var image1 = document.querySelector('.theme-toggler1');
-image1.addEventListener('click', toggleTheme);
